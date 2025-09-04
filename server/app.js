2889b9dc2cd00224ec9909ed22d355c4cb0912a8
niat-web <psm@nxtwave.in>
2025-09-04 10:09:48 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -15,8 +15,8 @@ dotenv.config();
 const app = express();
 
 // Body parser
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Enable CORS
 app.use(cors());
