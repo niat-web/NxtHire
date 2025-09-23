77c4b76ea261c3b6804e6e392870bede8add4a70
niat-web <psm@nxtwave.in>
2025-09-23 19:02:22 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,3 +1,27 @@
+// // client/vite.config.js
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import path from 'path'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, './src'),
+//     },
+//   },
+//   server: {
+//     port: 3000,
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:5000',
+//         changeOrigin: true,
+//       },
+//     },
+//   },
+// })
+
 // client/vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
@@ -17,6 +41,18 @@ export default defineConfig({
       '/api': {
         target: 'http://localhost:5000',
         changeOrigin: true,
+      },
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Creates a separate "vendor" chunk for all dependencies from node_modules
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
       },
     },
   },
