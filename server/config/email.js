532bd6442dc63b1dcb4b86ec15b55d5eb96b215d
niat-web <psm@nxtwave.in>
2025-09-25 22:37:05 +0530

    Update email.js

--- server/config/email.js
+++ server/config/email.js
@@ -1,39 +1,16 @@
-// // server/config/email.js
-// const nodemailer = require('nodemailer');
-
-// const createTransporter = () => {
-//   const transporter = nodemailer.createTransport({
-//     host: process.env.EMAIL_HOST,
-//     port: process.env.EMAIL_PORT,
-//     secure: process.env.EMAIL_SECURE === 'true',
-//     auth: {
-//       user: process.env.EMAIL_USER,
-//       pass: process.env.EMAIL_PASS
-//     }
-//   });
-  
-//   return transporter;
-// };
-
-// module.exports = {
-//   createTransporter
-// };
-
 // server/config/email.js
 const nodemailer = require('nodemailer');
 
 const createTransporter = () => {
-  // --- START MODIFICATION ---
-  // Using the built-in 'gmail' service is more reliable on hosting platforms
-  // as it handles connection details (host, port, security) automatically.
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    secure: process.env.EMAIL_SECURE === 'true',
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS
     }
   });
-  // --- END MODIFICATION ---
   
   return transporter;
 };
