2f5238667d469ec8ea59979b5635c89b901950d1
niat-web <psm@nxtwave.in>
2025-09-29 16:18:25 +0530

    Update email.js

--- server/config/email.js
+++ server/config/email.js
@@ -1,25 +1,3 @@
-// // server/config/email.js
-// const nodemailer = require('nodemailer');
-
-// const createTransporter = () => {
-//   const transporter = nodemailer.createTransport({
-//     host: process.env.EMAIL_HOST,
-//     port: process.env.EMAIL_PORT,
-//     secure: process.env.EMAIL_SECURE === 'true',
-//     auth: {
-//       user: process.env.EMAIL_USER,
-//       pass: process.env.EMAIL_PASS
-//     }
-//   });
-  
-//   return transporter;
-// };
-
-// module.exports = {
-//   createTransporter
-// };
-
-
 // server/config/email.js
 const nodemailer = require('nodemailer');
 
@@ -32,7 +10,6 @@ const createTransporter = () => {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS
     }
-    // REMOVE the tls: { rejectUnauthorized: false } block entirely.
   });
   
   return transporter;
@@ -41,3 +18,26 @@ const createTransporter = () => {
 module.exports = {
   createTransporter
 };
+
+
+// // server/config/email.js
+// const nodemailer = require('nodemailer');
+
+// const createTransporter = () => {
+//   const transporter = nodemailer.createTransport({
+//     host: process.env.EMAIL_HOST,
+//     port: process.env.EMAIL_PORT,
+//     secure: process.env.EMAIL_SECURE === 'true',
+//     auth: {
+//       user: process.env.EMAIL_USER,
+//       pass: process.env.EMAIL_PASS
+//     }
+//     // REMOVE the tls: { rejectUnauthorized: false } block entirely.
+//   });
+  
+//   return transporter;
+// };
+
+// module.exports = {
+//   createTransporter
+// };
