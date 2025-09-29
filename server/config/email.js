4b419c02375835f6ab000481ad8e2ac8a69e5f0b
niat-web <psm@nxtwave.in>
2025-09-29 11:43:23 +0530

    Update email.js

--- server/config/email.js
+++ server/config/email.js
@@ -1,3 +1,26 @@
+// // server/config/email.js
+// const nodemailer = require('nodemailer');
+
+// const createTransporter = () => {
+//   const transporter = nodemailer.createTransport({
+//     host: process.env.EMAIL_HOST,
+//     port: process.env.EMAIL_PORT,
+//     secure: process.env.EMAIL_SECURE === 'true',
+//     auth: {
+//       user: process.env.EMAIL_USER,
+//       pass: process.env.EMAIL_PASS
+//     }
+//   });
+  
+//   return transporter;
+// };
+
+// module.exports = {
+//   createTransporter
+// };
+
+
+
 // server/config/email.js
 const nodemailer = require('nodemailer');
 
@@ -9,7 +32,14 @@ const createTransporter = () => {
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS
+    },
+    // --- ADD THIS BLOCK TO BYPASS CERTIFICATE ISSUE ---
+    tls: {
+      // This is often required on cloud servers to prevent security blocks 
+      // when connecting to commercial SMTP like Gmail or Office 365.
+      rejectUnauthorized: false 
     }
+    // --- END ADDITION ---
   });
   
   return transporter;
