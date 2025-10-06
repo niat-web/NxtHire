c51b06b61c8b6d119abe65c71c138c0a8b5240cf
niat-web <psm@nxtwave.in>
2025-10-06 15:27:47 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,3 +1,30 @@
+// // client/src/main.jsx
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import { BrowserRouter as Router } from 'react-router-dom'
+// import App from './App.jsx'
+// import { AuthProvider } from './contexts/AuthContext.jsx'
+// import { AlertProvider } from './contexts/AlertContext.jsx'
+// import './assets/styles/index.css'
+// import { ToastContainer } from 'react-toastify'
+// import 'react-toastify/dist/ReactToastify.css'
+// import { registerServiceWorker } from './utils/sw-register.js'
+
+// registerServiceWorker(); // Register the service worker
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <Router>
+//       <AlertProvider>
+//         <AuthProvider>
+//           <App />
+//           <ToastContainer position="top-right" autoClose={5000} />
+//         </AuthProvider>
+//       </AlertProvider>
+//     </Router>
+//   </React.StrictMode>,
+// )
+
 // client/src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
@@ -18,7 +45,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <AlertProvider>
         <AuthProvider>
           <App />
-          <ToastContainer position="top-right" autoClose={5000} />
+          <ToastContainer position="top-right" autoClose={2000} />
         </AuthProvider>
       </AlertProvider>
     </Router>
