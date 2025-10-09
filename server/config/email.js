e7e6858979aeef1abae57c2faa3e0c8fe83dbeda
niat-web <psm@nxtwave.in>
2025-10-10 01:49:25 +0530

    Update email.js

--- server/config/email.js
+++ server/config/email.js
@@ -1,23 +1,35 @@
 // server/config/email.js
-const nodemailer = require('nodemailer');
-
-const createTransporter = () => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    secure: process.env.EMAIL_SECURE === 'true',
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS
-    }
-  });
-  
-  return transporter;
+const brevoConfig = {
+  apiUrl: 'https://api.brevo.com/v3/smtp/email',
+  apiKey: process.env.BREVO_API_KEY,
+  senderEmail: process.env.FROM_EMAIL,
+  senderName: process.env.FROM_NAME,
 };
 
 module.exports = {
-  createTransporter
+  brevoConfig,
 };
+
+// // server/config/email.js
+// const nodemailer = require('nodemailer');
+
+// const createTransporter = () => {
+//   const transporter = nodemailer.createTransport({
+//     host: process.env.EMAIL_HOST,
+//     port: process.env.EMAIL_PORT,
+//     secure: process.env.EMAIL_SECURE === 'true',
+//     auth: {
+//       user: process.env.EMAIL_USER,
+//       pass: process.env.EMAIL_PASS
+//     }
+//   });
+  
+//   return transporter;
+// };
+
+// module.exports = {
+//   createTransporter
+// };
 
 
 // // server/config/email.js
